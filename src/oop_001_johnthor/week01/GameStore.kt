ce88d6f34d8ed7f3855242e6722e8a4aebdad073
Kwazak <huanivan9@gmail.com>
2026-02-06 04:50:31 +0700

    week01: (tugas 2) implement null safety for user note

--- src/oop_001_johnthor/week01/GameStore.kt
+++ src/oop_001_johnthor/week01/GameStore.kt
@@ -10,12 +10,15 @@ fun main() {
     printReceipt(
         title = "Struk Belanja Toko Game Death Note",
         finalPrice = hasil,
+        userNote = null
     )
 }
 
 fun calculateDiscount(price: Int): Int = if (price > 500000) price * 80 / 100 else price * 90 / 100
-fun printReceipt(title: String, finalPrice: Int) {
+fun printReceipt(title: String, finalPrice: Int, userNote: String?) {
     println("=== $title ===")
     println("Total Harga: Rp $finalPrice")
+    val note = userNote ?: "Tidak ada catatan"
+    println("Catatan: $note")
 
 }
