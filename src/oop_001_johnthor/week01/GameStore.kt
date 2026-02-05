7c588cd44121925118df616a11f3893309ceda39
Kwazak <huanivan9@gmail.com>
2026-02-06 04:10:30 +0700

    (tugas 2) implement discount logic

--- src/oop_001_johnthor/week01/GameStore.kt
+++ src/oop_001_johnthor/week01/GameStore.kt
@@ -2,6 +2,9 @@ package oop_001_johnthor.week01
 
 fun main() {
     val gameTitle: String = "Red Dead Redemption 2"
-    val price: Int = 700000
+    val price: Int = 400000
     println("Nama: $gameTitle, Harga: $price")
+    println("Harga Seteah Diskon: ${calculateDiscount(price)}" )
 }
+
+fun calculateDiscount(price: Int): Int = if (price > 500000) price * 80 / 100 else price * 90 / 100
