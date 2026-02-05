d385f955de347aedb9e3a0e853814d3bdf256078
Kwazak <huanivan9@gmail.com>
2026-02-05 09:00:51 +0700

    week01: (tugas 1) refactor variables to val and use string

--- src/oop_001_johnthor/week01/CircleCalculator.kt
+++ src/oop_001_johnthor/week01/CircleCalculator.kt
@@ -1,21 +1,20 @@
 package oop_001_johnthor.week01
 
 fun main(args: Array<String>) {
-    var radius: Double = 7.0
-    var pi: Double = 3.14
+    val radius: Double = 7.0
+    val pi: Double = 3.14
 
-    var area: Double = pi * radius * radius
+    val area: Double = pi * radius * radius
 
-    println("Radius: "+ radius + ",Area: "+ area)
+    println("Radius: $radius ,Area: $area")
 
     checkSize(area)
 }
 
-fun checkSize(area: Double) {
-    if (area > 100) {
+fun checkSize(area: Double) = if (area > 100) {
         println("This is a Big Circle")
     } else {
         println("This is a Small Circle")
     }
-}
+
 
