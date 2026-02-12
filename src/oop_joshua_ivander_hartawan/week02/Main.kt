ac5ff9cfc393db316122fc32b5002ffe6490ade1
Kwazak <huanivan9@gmail.com>
2026-02-12 08:15:00 +0700

    week02: implement user input and pre-validation

---
+++ src/oop_joshua_ivander_hartawan/week02/Main.kt
@@ -0,0 +1,31 @@
+package oop_joshua_ivander_hartawan.week02
+
+import java.util.Scanner
+import oop_joshua_ivander_hartawan.week02.Student
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    println("--- APLIKASI PMB UMN ---")
+
+    print("Masukkan Nama: ")
+    val name = scanner.nextLine()
+
+    print("Masukkan NIM (Wajib 5 Karakter): ")
+    val nim = scanner.next()
+
+    scanner.nextLine()
+
+    // Validasi di sisi pemanggil (Main)
+    if (nim.length != 5) {
+        println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
+        // Program berhenti di sini untuk mahasiswa ini, tidak membuat objek
+    } else {
+        print("Masukkan Jurusan: ")
+        val major = scanner.nextLine()
+
+        // Instansiasi Objek karena data sudah aman
+        val s1 = Student(name, nim, major)
+        println("Status: Pendaftaran Selesai.")
+    }
+}
