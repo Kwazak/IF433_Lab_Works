9f09dbaff78e029585991c970ef1a56238bb8184
Kwazak <huanivan9@gmail.com>
2026-03-06 09:00:44 +0700

    week05: (task 1) test MathHelper overloading in main

--- src/oop_joshua_ivander_hartawan/week05/Main.kt
+++ src/oop_joshua_ivander_hartawan/week05/Main.kt
@@ -1,5 +1,16 @@
 package oop_joshua_ivander_hartawan.week05
 
+
+
+fun main() {
+    val helper = MathHelper()
+
+    println("Luas Persegi: ${helper.hitungLuas(5)}")            // Memanggil versi 1
+    println("Luas Persegi Panjang: ${helper.hitungLuas(5, 10)}") // Memanggil versi 2
+    println("Luas Lingkaran: ${helper.hitungLuas(7.0)}")         // Memanggil versi 3
+}
+
+/*
 fun main() {
     val dosen1 = Dosen( nama = "Pak Alex", nidn = "0123456" )
     val admin1 = Admin( nama = "Bu Sari" )
@@ -24,3 +35,7 @@ fun main() {
     }
 
 }
+*/
+
+
+
