2612ab8f8bdd1803cf9be4b9df5ab11f939c055e
Kwazak <huanivan9@gmail.com>
2026-03-27 08:30:00 +0700

    week07: (trial) demonstrate regular class equality failure

--- src/oop_joshua_ivander_hartawan/week07/Main.kt
+++ src/oop_joshua_ivander_hartawan/week07/Main.kt
@@ -1,11 +1,19 @@
 package oop_joshua_ivander_hartawan.week07
 
-fun main () {
-    println("=== TEST SINGLETON ===")
-    println("Status: ${DatabaseManager.connectionStatus}")
-    DatabaseManager.connect()
+//fun main () {
+//    println("=== TEST SINGLETON ===")
+//    println("Status: ${DatabaseManager.connectionStatus}")
+//    DatabaseManager.connect()
+//
+//    println("\n=== TEST COMPANION OBJECT ===")
+//    val client = NetworkClient.createClient()
+//    client.connect()
+//}
 
-    println("\n=== TEST COMPANION OBJECT ===")
-    val client = NetworkClient.createClient()
-    client.connect()
+fun main() {
+    println("\n=== TEST REGULAR CLASS ===")
+    val reg1 = RegularUser("Alice", 22)
+    val reg2 = RegularUser("Bob", 30)
+    println(reg1)
+    println("Sama? ${reg1 == reg2}")
 }
