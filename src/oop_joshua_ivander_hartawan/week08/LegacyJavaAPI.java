9b718fd82d7008a6410bfa281ba39fe9f8c2051b
Kwazak <huanivan9@gmail.com>
2026-04-17 08:03:31 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ src/oop_joshua_ivander_hartawan/week08/LegacyJavaAPI.java
@@ -0,0 +1,10 @@
+package oop_joshua_ivander_hartawan.week08;
+
+public class LegacyJavaAPI {
+
+    public static String fetchServerStatus() {
+            // Simulate fetching server status from a legacy API
+            return "SERVER_ONINE_200_OK.";
+
+    }
+}
