0c77dcc002a5037aca1dc5931acab1b15cbdaf95
Kwazak <huanivan9@gmail.com>
2026-04-17 08:08:45 +0700

    week08: implement java interop using !!

--- src/oop_joshua_ivander_hartawan/week07/Main.kt
+++ src/oop_joshua_ivander_hartawan/week07/Main.kt
@@ -35,6 +35,7 @@ fun main() {
     val uiMessage = when(response) {
         is ApiResponse.Success -> "Tampilkan: ${response.data}"
         is ApiResponse.Error -> "Munculkan alert: ${response.message}"
+        else -> {println("Hehey!!")}
     }
     println(uiMessage)
 
