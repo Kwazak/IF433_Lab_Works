333a25893df4642af6c4a53ef71459162c77fe15
Kwazak <huanivan9@gmail.com>
2026-04-17 08:18:48 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_joshua_ivander_hartawan/week08/JavaPaymentService.java
@@ -0,0 +1,8 @@
+package oop_joshua_ivander_hartawan.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
+
