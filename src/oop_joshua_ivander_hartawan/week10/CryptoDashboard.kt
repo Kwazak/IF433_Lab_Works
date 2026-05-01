c962c3c5908b3b244891f288ff6323665cb9b48b
Kwazak <huanivan9@gmail.com>
2026-05-01 23:41:16 +0700

    week10: (task) populate coin repository

--- src/oop_joshua_ivander_hartawan/week10/CryptoDashboard.kt
+++ src/oop_joshua_ivander_hartawan/week10/CryptoDashboard.kt
@@ -2,4 +2,8 @@ package oop_joshua_ivander_hartawan.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 65000.0))
+    coinRepo.add(Coin("ETH", 3500.0))
+    coinRepo.add(Coin("USDT", 1.0))
 }
