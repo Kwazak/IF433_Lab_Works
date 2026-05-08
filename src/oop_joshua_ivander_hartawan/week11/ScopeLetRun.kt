e2c9595a4898c6225f4169c84bcf3bfbde05eb1a
Kwazak <huanivan9@gmail.com>
2026-05-08 09:00:39 +0700

    week11: implement let function for null safety

---
+++ src/oop_joshua_ivander_hartawan/week11/ScopeLetRun.kt
@@ -0,0 +1,13 @@
+package oop_joshua_ivander_hartawan.week11
+
+fun main() {
+    println("=== TEST LET FUNCTION ===")
+    val name: String? = "Alexander"
+
+    val length = name?.let {
+        println("Nama terdeteksi: $it")
+        it.length
+    }
+    println("Panjang nama: $length")
+}
+
