ff90fc9ed1303497c4807cbdd034d1b1e581cc60
Kwazak <huanivan9@gmail.com>
2026-05-08 08:47:46 +0700

    week11: create string extension functions

---
+++ src/oop_joshua_ivander_hartawan/week11/StringExtension.kt
@@ -0,0 +1,10 @@
+package oop_joshua_ivander_hartawan.week11
+
+fun String.addGreeting(): String {
+    return "Hello, $this"
+}
+
+fun String.repeatTimes(n: Int): String {
+    return this.repeat(n)
+}
+
