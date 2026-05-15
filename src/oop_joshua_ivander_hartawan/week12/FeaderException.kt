c2e3e09340b0afac300194696c8a5df11f32a2f8
Kwazak <huanivan9@gmail.com>
2026-05-15 16:35:26 +0700

    week12: (task) create custom feeder exception subclasses

--- src/oop_joshua_ivander_hartawan/week12/FeaderException.kt
+++ src/oop_joshua_ivander_hartawan/week12/FeaderException.kt
@@ -3,3 +3,10 @@ package oop_joshua_ivander_hartawan.week12
 
 // Hierarchy Exception menggunakan Sealed Class
 sealed class FeederException(msg: String) : Exception(msg)
+
+// Subclass 1: Stok kibble habis/tidak cukup
+class FoodEmptyException(requested: Int, available: Int) :
+    FeederException("Kibble tidak cukup! Diminta $requested gr, sisa $available gr")
+
+// Subclass 2: Dispenser tersangkut/macet
+class DispenserJamException : FeederException("Wadah dispenser tersangkut/macet!")
