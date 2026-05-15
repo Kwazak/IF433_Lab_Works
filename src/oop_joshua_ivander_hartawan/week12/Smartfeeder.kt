0db8dcef62aa574393ce7ad602ff37233e5a1d95
Kwazak <huanivan9@gmail.com>
2026-05-15 16:38:26 +0700

    week12: (task) setup SmartFeeder main function

--- src/oop_joshua_ivander_hartawan/week12/Smartfeeder.kt
+++ src/oop_joshua_ivander_hartawan/week12/Smartfeeder.kt
@@ -17,5 +17,10 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
 
     return availableGram - requestedGram
 
+}
 
+fun main() {
+    var currentKibbleStock = 50
+    println("=== SMART PET FEEDER SYSTEM ===")
+    println("Stok kibble awal: $currentKibbleStock gr")
 }
