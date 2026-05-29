c3cc61d14849f91b9a595566eb569ee64ac29bd2
Kwazak <huanivan9@gmail.com>
2026-05-29 10:35:40 +0700

    week14: create DiscountStrategy interface and implementations

---
+++ src/oop_joshua_ivander_hartawan/week14/OcpRefactored.kt
@@ -0,0 +1,13 @@
+package oop_joshua_ivander_hartawan.week14
+
+interface DiscountStrategy {
+    fun apply(price: Double): Double
+}
+
+interface StudentDiscount : DiscountStrategy {
+    override fun apply(price: Double) = price * 0.80
+}
+
+interface MemberDiscount : DiscountStrategy {
+    override fun apply(price: Double) = price * 0.85
+}
