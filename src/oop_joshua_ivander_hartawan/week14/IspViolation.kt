1270aa0d2d6bf8fd1333aa0700f7a4894586e696
Kwazak <huanivan9@gmail.com>
2026-05-29 14:50:32 +0700

    week14: create fat MultifunctionDevice (ISP violation)

---
+++ src/oop_joshua_ivander_hartawan/week14/IspViolation.kt
@@ -0,0 +1,13 @@
+package oop_joshua_ivander_hartawan.week14
+
+interface MultifunctionDevice {
+    fun print(document: String)
+    fun scan(document: String)
+    fun fax(document: String)
+}
+
+class SimplePrinter : MultifunctionDevice {
+    override fun print(doc: String) = println("Printing: $doc")
+    override fun scan(document: String) = throw UnsupportedOperationException("No scanner")
+    override fun fax(document: String) = throw UnsupportedOperationException("No fax")
+}
