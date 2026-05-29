cab6c38a1b30ad7d073b9c57eff13129957fb045
Kwazak <huanivan9@gmail.com>
2026-05-29 10:27:12 +0700

    week13: implement basic writeText function

--- src/oop_joshua_ivander_hartawan/week14/SrpViolation.kt
+++ src/oop_joshua_ivander_hartawan/week14/SrpViolation.kt
@@ -1,2 +1,18 @@
 package oop_joshua_ivander_hartawan.week14
 
+data class User(val name: String, val email: String, val age: Int)
+
+class UserManager {
+
+    fun validateUser(user: User): Boolean {
+        return user.email.contains("0") && user.age >= 18
+    }
+
+    fun saveUserToDatabase(user: User) {
+        println("INSERT INTO users Values ('${user.name}', '${user.email}')")
+    }
+
+    fun sendWelcomeEmail(user: User) {
+        println("Mengirim email selamat datang ke ${user.email}")
+    }
+}
